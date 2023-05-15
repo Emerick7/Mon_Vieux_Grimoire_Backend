19e8af2487b6654b42b33292957b3a919849fc8b
Emerick Fouchard <fouchardemerick@gmail.com>
2023-05-15 11:13:00 +0200

    Auth protégée et fonctionnelle avec Token

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
+const jsonWebToken = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -28,7 +29,11 @@ exports.login = (req, res, next) => {
                         } else {
                             res.status(200).json({
                                 userId: user._id,
-                                token: 'TOKEN',
+                                token: jsonWebToken.sign(
+                                    {userId: user._id},
+                                    'RANDOM_TOKEN_SECRET',
+                                    {expiresIn: '24h'}
+                                )
                             });
                         }
                     })
--- routes/books.js
+++ routes/books.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const auth = require('auth');
 const router = express.Router();
 
 const booksControllers = require('../controllers/books');
 
-router.post('/', booksControllers.createBook);
-router.put('/:id', booksControllers.modifyBook);
-router.delete('/:id', booksControllers.deleteBook);
+router.post('/', auth, booksControllers.createBook);
+router.put('/:id', auth, booksControllers.modifyBook);
+router.delete('/:id', auth, booksControllers.deleteBook);
 router.get('/:id', booksControllers.getOneBook);
 router.get('/', booksControllers.getAllBooks);
 router.get('/bestrating', booksControllers.getBestRatingBooks);
-router.post('/:id/rating', booksControllers.postRatingBook);
+router.post('/:id/rating', auth, booksControllers.postRatingBook);
 
 module.exports = router;
